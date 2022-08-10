d99b775c62721c5d1ba219460f692452ddbd2fc9
Matevos Grigoryan <matevos.grigoryan@technamin.com>
2022-08-10 18:39:57 +0400

    added discount api

---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.API.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string prodictName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string prodictName);
+    }
+}
