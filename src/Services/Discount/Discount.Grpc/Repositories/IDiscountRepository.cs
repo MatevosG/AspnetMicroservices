b1d4011e4d4e42f6be21c75c21e5375ce39ba1fb
Matevos Grigoryan <matevos.grigoryan@technamin.com>
2022-08-14 16:44:01 +0400

    Worked Grpc

---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Grpc.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string prodictName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string prodictName);
+    }
+}
