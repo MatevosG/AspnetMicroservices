f8de7d3da55c29a91adbc1351526e6fd688c2797
Matevos Grigoryan <matevos.grigoryan@technamin.com>
2022-08-24 13:55:11 +0400

    worked in orderingapi

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ordering.Application.Exceptions
+{
+    internal class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key) 
+            : base($"Entity\"{name}\" ({key}) was not found")
+        {
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -5,7 +5,7 @@ namespace Ordering.Domain.Common
     public abstract class EntityBase
     {
         public int Id { get; protected set; }
-        public string CreateBy { get; set; }
+        public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public string LastModifiedBy { get; set; }
         public DateTime? LastModifiedDate { get; set; }
